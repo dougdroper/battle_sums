0f5626d650b6350ccbf7be36b8fe36096bb0f467
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-03-22 22:51:45 +0000

    Set up socket.io

--- app.js
+++ app.js
@@ -4,9 +4,11 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , sio = require('socket.io');
 
 var app = module.exports = express.createServer();
+
 
 // Configuration
 
@@ -31,5 +33,34 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
+
+
+function getRandomNumber(){
+  return Math.round(Math.random(12) * 10);
+};
+
+function getRandomOperator(){
+  var val = getRandomNumber();
+  switch(true){
+    case (val < 4):
+      return "+";
+      break;
+    case (val >= 4 && val < 6):
+      return "*";
+      break;
+    case (val >= 6 && val < 8):
+      return "/";
+      break;
+    default:
+      return "-";
+      break;
+  }
+}
+
 app.listen(3000);
+var io = sio.listen(app);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('setup', { first: getRandomNumber(), second: getRandomNumber(), operator: getRandomOperator() });
+});
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
