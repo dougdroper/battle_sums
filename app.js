9ad4228de07012e5a8bc189a319e71b69ec27c21
Douglas Roper <douglasroper@notonthehighstreet.com>
2012-03-23 15:51:02 +0000

    List users, and send request to !battle!, also accept bettle

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var express = require('express')
   , sum = require('./lib/random_sum_generator');
 
 var f, s, o, answer;
+var users = [];
+var clients = {};
 
 var app = module.exports = express.createServer();
 
@@ -49,10 +51,45 @@ function setup(){
 }
 
 io.sockets.on('connection', function (socket) {
-  setup();
+  socket.on('user', function (data, fn) {
+    if(users.indexOf(data) != -1) {
+      fn(false);
+    } else {
+      fn(true);
+      users.push(data);
+      socket.user = data;
+      clients[socket.user] = socket;
+      socket.broadcast.emit('users', { users : users })
+    }
+  });
 
-  socket.emit("setup", { first: f, second: s, operator: o });
-  socket.broadcast.emit('setup', { first: f, second: s, operator: o });
+  socket.on('users', function() {
+    socket.emit('users', { users : users })
+  })
+
+  socket.on('disconnect', function () {
+    if (!socket.user) return;
+    users.splice(users.indexOf(socket.user), 1);
+    socket.broadcast.emit('users', { users : users })
+  });
+
+  socket.on("battle_user", function(data, fn) {
+    if (!socket.user && users.indexOf(data) != -1) {
+      fn(false);
+    } else{
+      fn(true);
+      clients[data].emit('request_battle', { requester : socket.user })
+    };
+  });
+
+  socket.on('begin_battle', function (data, fn) {
+    setup();
+    socket.emit("setup", { first: f, second: s, operator: o });
+    clients[data].emit('setup', { first: f, second: s, operator: o });
+  });
+
 });
+
+
 
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
